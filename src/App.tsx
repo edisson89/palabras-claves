063689a74f3eaf1cf33847a456ee96c9c8e3957c
edisson89 <edisson.12@hotmail.com>
2023-09-03 23:28:29 -0500

    Refactorizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,23 @@
-import { useState } from "react";
-import "./App.module.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
-  const [word, setWord] = useState("");
-  
-  function getTopWords(text: string) {
-    // Limpiar el texto y convertirlo a minúsculas
-    const cleanedText = text.toLowerCase().replace(/[^\w\s]/g, " ");
-
-    // Obtener todas las palabras del texto en un array
-    const words: Array<string> = cleanedText.split(/\s+/);
-
-    // Contar la frecuencia de cada palabra
-    const wordCount: Record<string, number> = {};
-    words.forEach((word) => {
-      if (word.length > 3) {
-        if (wordCount[word]) {
-          wordCount[word]++;
-        } else {
-          wordCount[word] = 1;
-        }
-      }
-    });
-
-    // Ordenar las palabras por su frecuencia en orden descendente
-    const sortedWords = Object.keys(wordCount).sort(
-      (a, b) => wordCount[b] - wordCount[a]
-    );
-
-    // Obtener las 10 palabras más repetidas, incluyendo la palabra clave del título
-    const topWords = sortedWords.slice(0, 10);
-
-    return topWords;
-  }
-
-  function countWords() {
-    const topWords = getTopWords(word);
-    const wordList = topWords.map((word) => <li key={word}>{word}</li>);
-
-    return (
-      <div>
-        <p>Las 10 palabras clave son:</p>
-        <ul>{wordList}</ul>
-      </div>
-    );
-  }
-
+  const location = useLocation()
   return (
-    <>
-      <h1>Optimice SEO con palabras claves</h1>
-      <h2>Ingrese su texto</h2>
-      <div className="parrafo">
-        <textarea
-          id="input-text"
-          onChange={(e) => setWord(e.target.value)}
-          value={word}
-          rows={25}
-          cols={80}
-          placeholder="texto"
-        ></textarea>
-        <button onClick={() => countWords()}>Palabras clave</button>
-      </div>
+    <div>
+     
+       {location.pathname !== "/" ? (
+        <Home  />
+      ) : (
+        <Home />
+      )}
 
-      <div id="output">{countWords()}</div>
-    </>
+       <Routes >
+        <Route path="/home" element={<Home />} />
+       </Routes>
+      
+      
+    </div>
   );
 }
 
